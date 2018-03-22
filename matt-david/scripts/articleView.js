9ae825cea9df7645be9ef5053438f531a9cbac1a
Matthew Gordon <matthewgordon89@gmail.com>
2018-03-21 20:01:30 -0700

    Populated article selections

--- matt-david/scripts/articleView.js
+++ matt-david/scripts/articleView.js
@@ -27,7 +27,7 @@ articleView.populateFilters = function() {
 
       // TODO: Refactor this concatenation using a template literal.
       optionTag = '<option value="' + category + '">' + category + '</option>';
-      optionTag = `<option value= ${category}> ${category}<option>`
+      optionTag = `<option value= ${category}>${category}<option>`
 
       if ($('#category-filter option[value="' + category + '"]').length === 0) {
         $('#category-filter').append(optionTag);
@@ -35,12 +35,17 @@ articleView.populateFilters = function() {
     }
   });
 };
-
+articleView.populateFilters();
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
     if ($(this).val()) {
-      // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
+      $('#articles').hide();
+    
+      $('img[data-flavor="' + $selection + '"]').show()
+      // TODO: If the <select> menu was changed to an option that has a value, 
+      //we first need to hide all the articles, and then 
+      //show just the ones that match for the author that was selected.
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
 
     } else {
